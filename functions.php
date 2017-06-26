7590a4da2735405ae7361c90a2293c4cd2dc6d50
Joshua Matthews <joshuamatthews906@yahoo.com>
2017-06-26 10:46:53 -0500

    font files and bootstrap added

--- functions.php
+++ functions.php
@@ -88,6 +88,7 @@ require_once( get_template_directory() . '/inc/include.php' );
  */
 function oneline_lite_scripts() {
   // Add Genericons font, used in the main stylesheet.
+	wp_enqueue_style( 'bootstrap_min_css', get_template_directory_uri() . '/css/bootstrap.min.css' );
   wp_enqueue_style( 'animate', get_template_directory_uri() . '/css/animate.css', array(), '1.0.0' );
   wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/css/font-awesome/css/font-awesome.css', array(), '1.0.0' );
 wp_enqueue_style( 'bx-slider', get_template_directory_uri() . '/css/bxslider.css', array(), '1.0.0' );
